d7fee73df48209022b487e8d7a10a561608a2fb8
Antonio López Soult <antlopsou@gmail.com>
2024-09-03 17:31:44 +0200

    add Hero section

---
+++ components/ui/DotBackground.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function DotBackground() {
+  return (
+
+    <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.09]
+     bg-dot-black-100/[0.09] absolute top-0 left-0 flex items-center justify-center">
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center 
+      dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    </div>
+  );
+}
